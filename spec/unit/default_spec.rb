8c950c9de5548c2b245c311f70f4f42df524933c
Dan Wendorf <dwendorf@pivotal.io>
2016-05-31 14:24:45 -0700

    Stub 'which git' in unit specs

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -3,6 +3,10 @@ require 'unit/spec_helper'
 describe 'sprout-android::default' do
   let(:runner) { ChefSpec::SoloRunner.new }
 
+  before do
+    stub_command('which git')
+  end
+
   it 'includes the sdk recipe' do
     runner.converge(described_recipe)
     expect(runner).to include_recipe('sprout-android::sdk')
--- spec/unit/haxm_spec.rb
+++ spec/unit/haxm_spec.rb
@@ -3,6 +3,10 @@ require 'unit/spec_helper'
 describe 'sprout-android::haxm' do
   let(:runner) { ChefSpec::SoloRunner.new }
 
+  before do
+    stub_command('which git')
+  end
+
   it 'depends on the sdk recipe' do
     runner.converge(described_recipe)
     expect(runner).to include_recipe('sprout-android::sdk')
